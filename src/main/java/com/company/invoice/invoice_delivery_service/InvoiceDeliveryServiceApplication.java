cacb405629c1e9a33784e3f0c0648c8d2c14a30f
rakesh4online <rakesh4online@gmail.com>
2024-11-10 20:33:18 +0530

    Second commit

---
+++ src/main/java/com/company/invoice/invoice_delivery_service/InvoiceDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.invoice.invoice_delivery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceDeliveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceDeliveryServiceApplication.class, args);
+	}
+
+}
